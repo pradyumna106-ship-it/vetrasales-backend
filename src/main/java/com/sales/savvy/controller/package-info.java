6a45a2930d306c279a96d046ea992b0b48ce6c87
pr4dyumn4 <pradyumnajekumar@gmail.com>
2025-11-04 12:13:13 +0530

    deleted again

--- src/main/java/com/sales/savvy/controller/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.sales.savvy.controller;
--- src/main/java/com/sales/savvy/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sales.savvy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sales.savvy.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
